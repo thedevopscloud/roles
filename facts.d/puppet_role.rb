3e0b9de1232ffcb0b7fbfcc94a3d6a7fc24accb8
thedevopscloud <43915910+thedevopscloud@users.noreply.github.com>
2018-11-29 19:06:03 +0530

    Delete puppet_role.rb

--- facts.d/puppet_role.rb
+++
@@ -1,5 +0,0 @@
-Facter.add('role') do
-  setcode do
-    Facter::Core::Execution.execute('/usr/bin/cat /opt/puppetlabs/puppet/cache/state/classes.txt | grep -i roles | awk -F:: '{print $2"::"$3}'')
-  end
-end
