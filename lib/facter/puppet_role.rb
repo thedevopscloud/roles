eb8e8069fde44d97298bd39635871d3ded5745af
thedevopscloud <43915910+thedevopscloud@users.noreply.github.com>
2018-11-29 21:05:55 +0530

    Update puppet_role.rb

--- lib/facter/puppet_role.rb
+++ lib/facter/puppet_role.rb
@@ -1,5 +1,5 @@
 Facter.add('role') do
   setcode do
-    Facter::Core::Execution.execute('/usr/bin/cat /opt/puppetlabs/puppet/cache/state/classes.txt | grep -i roles | awk -F:: '{print $2"::"$3}'')
+    Facter::Core::Execution.execute('/usr/bin/cat /opt/puppetlabs/puppet/cache/state/classes.txt | grep -i roles  | cut -c8- ')
   end
 end
